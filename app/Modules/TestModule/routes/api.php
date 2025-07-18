a3cfe84676bc52928d5e59e740afaa9d11460433
zoran <zoran.bogoevski@iwinback.com>
2025-07-18 16:48:45 +0200

    remove test module

--- app/Modules/TestModule/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Modules\TestModule\Http\Controllers\TestModuleController;
-
-Route::prefix('api/v1')->group(function () {
-    Route::apiResource('test_modules', TestModuleController::class);
-});
